23d8c83dea2f333fa32c9b1006f6ea4726faf42b
Anton Stremousov <stremousoff@gmail.com>
2024-07-17 03:45:06 +0300

    build_and_push_to_docker_hub

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['practicum-infra.ddns.net', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
